1182d978a4da988455a2981dfb741599fb56bf03
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-08 01:58:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,13 +85,13 @@ def adding_matz
       :known_for => "Ruby",
       :languages => ["LISP", "C"]
     }
-    programmer_hash  
+    programmer_hash
 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
   # return the entire updated hash
-  
+
 	programmer_hash =
  		{
       :grace_hopper => {
